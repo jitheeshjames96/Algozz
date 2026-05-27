777e7ebc93275a006a3bec53de00d684289195e8
Jitheesh <jitheeshjames96@gmail.com>
2026-05-27 18:28:08 +0530

    chore: update api rewrite destination to the new EC2 IP 3.108.226.70

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://65.2.55.114:8000/api/:path*',
+        destination: 'http://3.108.226.70:8000/api/:path*',
       },
     ];
   },
